95ce0e0ae9ca312659e334bd511337db1286226f
ufairooh <ufairooh@gmail.com>
2019-12-17 12:35:14 +0700

    semua crud selesai

--- login.php
+++ login.php
@@ -1,86 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Login Page</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
-	
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
-				<form class="login100-form validate-form" method="post" action="login_function.php">
-					<span class="login100-form-title p-b-33">
-						Welcome Admin
-					</span>
+<?php
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email" required>
-						<span class="focus-input100-1"></span>
-						<span class="focus-input100-2"></span>
-					</div>
+//login.php
 
-					<div class="wrap-input100 rs1 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password" required>
-						<span class="focus-input100-1"></span>
-						<span class="focus-input100-2"></span>
-					</div>
+include('config2.php');
 
-					<div class="container-login100-form-btn m-t-20">
-						<button name="submit" type="submit" class="login100-form-btn">
-							Sign In
-						</button>
-					</div>
+session_start();
 
-					
+$form_data = json_decode(file_get_contents("php://input"));
 
-					
-				</form>
-			</div>
-		</div>
-	</div>
-	
+$validation_error = '';
 
-	
-<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+if(empty($form_data->username))
+{
+ $error[] = 'username is Required';
+}
+else
+{
+ if(!filter_var($form_data->username))
+ {
+  $error[] = 'Invalid username Format';
+ }
+ else
+ {
+  $data[':username'] = $form_data->username;
+ }
+}
 
-</body>
-</html>
+if(empty($form_data->password))
+{
+ $error[] = 'Password is Required';
+}
+
+if(empty($error))
+{
+ $query = "
+ SELECT * FROM account 
+ WHERE emai_admin = :username
+ ";
+ $statement = $connect->prepare($query);
+ if($statement->execute($data))
+ {
+  $result = $statement->fetchAll();
+  if($statement->rowCount() > 0)
+  {
+   foreach($result as $row)
+   {
+    if(password_verify($form_data->password, $row["password"]))
+    {
+     $_SESSION["nama"] = $row["nama_admin"];
+    }
+    else
+    {
+     $validation_error = 'Wrong Password';
+    }
+   }
+  }
+  else
+  {
+   $validation_error = 'Wrong Username';
+  }
+ }
+}
+else
+{
+ $validation_error = implode(", ", $error);
+}
+
+$output = array(
+ 'error' => $validation_error
+);
+
+echo json_encode($output);
+
+?>
